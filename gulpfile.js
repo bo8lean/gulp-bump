840dc44f0a23b8f0d7a62b36369188ea89b53cdb
Bala Thavapalan <thavapalan.balasanmuganathan@ingdirect.com.au>
2017-11-29 15:44:50 +1100

    Gulp bump in use

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
-
-const gulp = require('gulp'),
+const
+    gulp = require('gulp'),
+    git = require('gulp-git'),
     bump = require('gulp-bump')
+    filter = require('gulp-filter'),
+    tag_version = require('gulp-tag-version')
     ;
 
 
 function inc(importance) {
-// get the files to bump version in 
-return gulp.src(['./package.json'])
-    
+    // get the files to bump version in 
+    return gulp.src(['./package.json'])
+
     // bump the version number in those files 
     .pipe(bump({type: importance}))
-    
+
     // save it back to filesystem 
     .pipe(gulp.dest('./'))
+
+    // commit the changed version number 
+    .pipe(git.commit('bumps package version'))
+
+    // read only one file to get the version number 
+    .pipe(filter('package.json'))
+
+    // **tag it in the repository** 
+    .pipe(tag_version());
 }    
 
 gulp.task('bump', function() { return inc('patch'); })
